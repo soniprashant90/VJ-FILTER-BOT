d2f524c8007506d2898b4103701320c6e8348c29
soniprashant90 <soniprashant671@gmail.com>
2026-01-16 00:09:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://i.ibb.co/MC9cC5f/IMG-20241205-185040-352.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/DPngQS9L/photo-2026-01-16-00-02-20.jpg')).split()
 
 
 # Admins & Users
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
